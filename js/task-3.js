d6ce621fb9a190940de4f96149fb53219460ab28
Nastyaashh <anastasiya15012006@gmail.com>
2024-10-01 00:25:11 +0300

    did js-1 and js-2

---
+++ js/task-3.js
@@ -0,0 +1,17 @@
+// class StringBuilder {
+//   #value;
+
+//   constructor(value) {
+//     this.#value = value;
+//   }
+
+//   getValue() {
+//     return this.#value;
+//   }
+
+//   padEnd(str) {
+//     this.#value.push(str);
+//   }
+
+//   padStart(str) {}
+// }
