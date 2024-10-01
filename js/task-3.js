d4cf2be01a85a01b95158a1c5122fc8faa591bd1
Nastyaashh <anastasiya15012006@gmail.com>
2024-10-01 07:49:06 +0300

    finished js-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,17 +1,33 @@
-// class StringBuilder {
-//   #value;
+class StringBuilder {
+  #value;
 
-//   constructor(value) {
-//     this.#value = value;
-//   }
+  constructor(initialValue) {
+    this.#value = initialValue;
+  }
 
-//   getValue() {
-//     return this.#value;
-//   }
+  getValue() {
+    return this.#value;
+  }
 
-//   padEnd(str) {
-//     this.#value.push(str);
-//   }
+  padEnd(str) {
+    this.#value += str;
+  }
 
-//   padStart(str) {}
-// }
+  padStart(str) {
+    this.#value = str + this.#value;
+  }
+
+  padBoth(str) {
+    this.padStart(str);
+    this.padEnd(str);
+  }
+}
+
+const builder = new StringBuilder('.');
+console.log(builder.getValue()); // "."
+builder.padStart('^');
+console.log(builder.getValue()); // "^."
+builder.padEnd('^');
+console.log(builder.getValue()); // "^.^"
+builder.padBoth('=');
+console.log(builder.getValue()); // "=^.^="
